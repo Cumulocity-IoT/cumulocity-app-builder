6825738a979a0b55649e72670b0128cbfb780162
Richard Peach (RPEA) <Richard.Peach@softwareag.com>
2020-01-30 10:42:20 +0000

    Re-adding the simulator code

--- app.module.ts
+++ app.module.ts
@@ -22,16 +22,16 @@ import {NavigationError, Router, RouterModule as NgRouterModule} from '@angular/
 import {UpgradeModule as NgUpgradeModule} from '@angular/upgrade/static';
 import {AppStateService, CoreModule, HOOK_NAVIGATOR_NODES, RouterModule} from '@c8y/ngx-components';
 import { UpgradeModule, HybridAppModule, UPGRADE_ROUTES } from '@c8y/ngx-components/upgrade';
-// import {DeviceSimulatorConfigModule} from "./device-simulator-config/device-simulator-config.module";
+import {DeviceSimulatorConfigModule} from "./device-simulator-config/device-simulator-config.module";
 import {ApplicationBuilderModule} from "./application-builder/application-builder.module";
 import {ApplicationModule} from "./application/application.module";
 import {ConfigNavigationService, Navigation} from "./navigation";
-// import {DeviceSimulatorConfigComponent} from "./device-simulator-config/device-simulator-config.component";
+import {DeviceSimulatorConfigComponent} from "./device-simulator-config/device-simulator-config.component";
 import {BrandingComponent} from "./branding/branding.component";
 import {BrandingModule} from "./branding/branding.module";
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import {BrandingDirtyGuardService} from "./branding/branding-dirty-guard.service";
-// import {FixedValueSimulationStrategyModule} from "./simulation-strategies/fixed-value/fixed-value.simulation-strategy.module";
+import {FixedValueSimulationStrategyModule} from "./simulation-strategies/fixed-value/fixed-value.simulation-strategy.module";
 import {HelpComponent} from "./help/help.component";
 import {MarkdownModule} from "ngx-markdown";
 import {CustomWidgetsModule} from "./custom-widgets/custom-widgets.module";
@@ -47,10 +47,10 @@ import {IUser} from '@c8y/client';
     BrowserAnimationsModule,
     RouterModule.forRoot(),
     NgRouterModule.forRoot([
-      // {
-      //   path: 'application/:applicationId/simulator-config',
-      //   component: DeviceSimulatorConfigComponent
-      // },
+      {
+        path: 'application/:applicationId/simulator-config',
+        component: DeviceSimulatorConfigComponent
+      },
       {
         path: 'application/:applicationId/branding',
         component: BrandingComponent,
@@ -67,8 +67,8 @@ import {IUser} from '@c8y/client';
     CustomWidgetsModule,
     ApplicationModule,
     BrandingModule.forRoot(),
-    // DeviceSimulatorConfigModule,
-    // FixedValueSimulationStrategyModule,
+    DeviceSimulatorConfigModule,
+    FixedValueSimulationStrategyModule,
     MarkdownModule.forRoot(),
     NgUpgradeModule,
     // Upgrade module must be the last
