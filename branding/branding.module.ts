3403bb9ccb6246d1f0df7ecdaa38a0d393ad9ad2
RPEA <richard.peach@softwareag.com>
2020-02-12 22:17:35 +0000

    Adding AppIdService to simplify getting the application id from the URL

--- branding/branding.module.ts
+++ branding/branding.module.ts
@@ -21,13 +21,13 @@ import {ModuleWithProviders, NgModule} from "@angular/core";
 import {BrandingComponent} from "./branding.component";
 import {CommonModule} from "@angular/common";
 import {BrandingService} from "./branding.service";
-import {distinctUntilChanged, filter, map, mapTo, switchMap, first} from "rxjs/operators";
-import {ActivationEnd, Router} from "@angular/router";
+import {switchMap} from "rxjs/operators";
 import {from, of} from "rxjs";
 import {ApplicationService} from "@c8y/client";
-import {AppStateService, CoreModule} from "@c8y/ngx-components";
+import {CoreModule} from "@c8y/ngx-components";
 import {BrandingDirtyGuardService} from "./branding-dirty-guard.service";
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import {AppIdService} from "../app-id.service";
 
 @NgModule({
     imports: [
@@ -40,36 +40,14 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
     ]
 })
 export class BrandingModule {
-    constructor(router: Router, appService: ApplicationService, brandingService: BrandingService, appStateService: AppStateService) {
-        router.events
-            .pipe(
-                filter(event => event instanceof ActivationEnd),
-                map((event: ActivationEnd) => event.snapshot.url),
-                // Delay until after login
-                switchMap(url => appStateService.currentUser.pipe(
-                    filter(user => user != null),
-                    first(),
-                    mapTo(url)
-                )),
-                switchMap(url => {
-                    if (url.length >= 2 && url[0].path === 'application') {
-                        const appId = url[1].path;
-                        return from(appService.detail(appId))
-                            .pipe(
-                                map(res => res.data as any)
-                            );
-                    } else {
-                        return of(undefined);
-                    }
-                }),
-                distinctUntilChanged((prev, curr) => {
-                    if (prev == undefined || curr == undefined) {
-                        return prev == undefined && curr == undefined;
-                    } else {
-                        return prev.id == curr.id;
-                    }
-                })
-            )
+    constructor(appIdService: AppIdService, appService: ApplicationService, brandingService: BrandingService) {
+        appIdService.appIdDelayedUntilAfterLogin$.pipe(switchMap(appId => {
+            if (appId != undefined) {
+                return from(appService.detail(appId).then(res => res.data as any));
+            } else {
+                return of(undefined);
+            }
+        }))
             .subscribe(app => {
                 brandingService.updateStyleForApp(app);
             });
