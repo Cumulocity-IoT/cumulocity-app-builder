b7f225615eefcab603aea492d014643a8f9d28a2
RPEA <richard.peach@softwareag.com>
2020-08-13 10:05:55 +0100

    Adding missing ability to change app name, icon and context path

--- builder/application-config/dashboard-config.component.ts
+++ builder/application-config/dashboard-config.component.ts
@@ -27,6 +27,9 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import {NewDashboardModalComponent} from "./new-dashboard-modal.component";
 import {EditDashboardModalComponent} from "./edit-dashboard-modal.component";
 import {AppIdService} from "../app-id.service";
+import {UpdateableAlert} from "../utils/UpdateableAlert";
+import {contextPathFromURL} from "../utils/contextPathFromURL";
+import * as delay from "delay";
 
 export interface DashboardConfig {
     id: string,
@@ -105,8 +108,59 @@ export class DashboardConfigComponent implements OnDestroy {
     }
 
     async saveAppChanges(app) {
-        // TODO
-        throw new Error("Not implemented");
+        const savingAlert = new UpdateableAlert(this.alertService);
+
+        savingAlert.update('Saving application...');
+
+        try {
+            app.name = this.newAppName;
+            app.applicationBuilder.icon = this.newAppIcon;
+            app.icon = {
+                name: this.newAppIcon,
+                "class": `fa fa-${this.newAppIcon}`
+            };
+
+            const update: any = {
+                id: app.id,
+                name: app.name,
+                key: `application-builder-${app.name}-app-key`,
+                applicationBuilder: app.applicationBuilder,
+                icon: app.icon
+            };
+
+            if (app.manifest) {
+                app.manifest.icon = app.icon;
+                update.manifest = app.manifest;
+            }
+
+            let contextPathUpdated = false;
+            const currentAppContextPath = app.contextPath;
+            if (app.contextPath && app.contextPath != this.newAppContextPath) {
+                app.contextPath = this.newAppContextPath;
+                update.contextPath = this.newAppContextPath;
+                contextPathUpdated = true;
+            }
+
+            await this.appService.update(update);
+
+            if (contextPathUpdated && contextPathFromURL() === currentAppContextPath) {
+                savingAlert.update('Saving application...\nWaiting for redeploy...');
+                // Pause while c8y server reloads the application
+                await delay(5000);
+                window.location = `/apps/${this.newAppContextPath}/${window.location.hash}` as any;
+            }
+
+            savingAlert.update('Application saved!', 'success');
+            savingAlert.close(1500);
+        } catch(e) {
+            savingAlert.update('Unable to save!\nCheck browser console for details', 'danger');
+            throw e;
+        }
+
+        // Refresh the application name/icon
+        this.brandingService.updateStyleForApp(app);
+        // Refresh the applications list
+        this.appStateService.currentUser.next(this.appStateService.currentUser.value);
     }
 
     showCreateDashboardDialog(app) {
