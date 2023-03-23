2ef5d20bf457e69f0455db841790a87bd56f37b4
Sheethal JR <sjr@softwareag.com>
2023-03-23 16:47:40 +0530

    Changes while uninstalling widgets

--- builder/app-builder-upgrade/app-builder-upgrade.service.ts
+++ builder/app-builder-upgrade/app-builder-upgrade.service.ts
@@ -509,7 +509,7 @@ export class AppBuilderUpgradeService {
     private async uninstallWidgets(plugins: any) {
         const appList = await this.getApplicationList();
         for (const pluginBinary of plugins) {
-            const widgetAppObj = appList.find(app => app.contextPath === pluginBinary.contextPath)
+            const widgetAppObj = appList.find(app => pluginBinary.oldContextPath ? app.contextPath === pluginBinary.oldContextPath : app.contextPath === pluginBinary.contextPath)
             if (widgetAppObj) {
                 try {
                     await this.appService.delete(widgetAppObj.id);
